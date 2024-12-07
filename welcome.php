b827bd0c8bdbd213427fd72528d580b94ee55f1f
Arthur <153740966+Buddhinha@users.noreply.github.com>
2024-12-07 13:27:36 -0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,9 +1,46 @@
+<?php 
+session_start();
+
+if($_COOKIE['tema'] == "escuro"){
+$style = "
+body{
+    background-color:black;
+    color:white;
+    
+}";
+} else{
+    $style = "
+    body{
+        background-color:white;
+        color:black;
+    }";   
+}
+
+
+?>
+
+
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Bem-vindo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bem vindo</title>
+    <style>
+        <?= $style; ?>
+        a{
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
-<h1>Bem-vindo!</h1>
+    welocome! <br>
+    <?php 
+    echo $_SESSION['usuario'];
+    ?> 
+    <br>
+    <button><a href="logout.php">deslogar</a></button>
 </body>
 </html>
+
